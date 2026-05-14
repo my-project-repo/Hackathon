479c2654994220547f823bf0e585346cbc8ea110
Chinmaya-Meher <chinmaya.meher.bca27@tha.edu.in>
2026-05-14 20:30:43 +0530

    sanitizeCode.js added

---
+++ engine/sanitizeCode.js
@@ -0,0 +1,29 @@
+function sanitizeCode(code) {
+
+  if (!code) {
+    return "";
+  }
+
+  // Remove weird Monaco arrows
+  code = code.replace(/›/g, "");
+
+  // Remove isolated line numbers
+  code = code.replace(
+    /^\s*\d+\s*$/gm,
+    ""
+  );
+
+  // Remove accidental standalone */
+  code = code.replace(
+    /^\s*\*\/\s*$/gm,
+    ""
+  );
+
+  // Normalize spaces
+  code = code.replace(
+    /\u00A0/g,
+    " "
+  );
+
+  return code.trim();
+}
